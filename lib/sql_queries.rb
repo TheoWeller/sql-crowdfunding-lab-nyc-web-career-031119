7dac15de9c607651e4d8a0dfcf7aeb252ea98bf4
Theo Weller <weller.theodor@gmail.com>
2019-03-20 18:19:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,11 +24,11 @@ GROUP BY users.name;
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS goal_met
 FROM projects
 JOIN pledges
 ON projects.id = pledges.project_id
-WHERE SUM(pledges.amount) >= projects.funding_goal;"
+WHERE goal_met >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
